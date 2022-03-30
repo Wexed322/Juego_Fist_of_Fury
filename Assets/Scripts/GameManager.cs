c2be7581c95ae1996c3dc2f6a133ab901c4ca9e6
JostiN322 <jostinoriguela@gmail.com>
2022-03-30 12:00:34 -0500

    gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance_;
+    private void Awake()
+    {
+        if (GameManager.instance_ == null)
+        {
+            GameManager.instance_ = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
