394a43ec7fca313eeb060c2955b33d163f2b98ec
JostiN322 <jostinoriguela@gmail.com>
2022-04-13 15:50:40 -0500

    dontDestroy

---
+++ Assets/Scripts/Main_Menu/BotonInicio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BotonInicio : MonoBehaviour
+{
+    public Button loadSceneGameplay;
+    public Slider loadSlider;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    public void loadSceneGame() 
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ActivateLoadingCanvas() 
+    {
+        StartCoroutine(LoadLevel());
+    }
+    IEnumerator LoadLevel() 
+    {
+        //yield return new WaitForSeconds(1);
+        AsyncOperation op = SceneManager.LoadSceneAsync("Game");
+        loadSceneGameplay.gameObject.SetActive(false);
+        loadSlider.gameObject.SetActive(true);
+
+        while (!op.isDone)
+        {
+            loadSlider.value = op.progress;
+            yield return null;
+        }
+
+    }
+
+}
